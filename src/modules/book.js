84bc593889ff5b4fbd3b9b46fc7d496ccabf718b
Camilovelag <camilo.vela.galindo@gmail.com>
2022-09-26 11:19:20 -0500

    Modularize navigation bar feature

---
+++ src/modules/book.js
@@ -0,0 +1,9 @@
+const listBooks = document.querySelector('.list-books');
+const booksContainer = document.querySelector('.books-wrapper');
+const form = document.querySelector('.form-input');
+const contact = document.querySelector('.contact');
+const [title, author] = form.elements;
+
+const listPage = document.querySelector('.list-page');
+const addNewPage = document.querySelector('.add-new-page');
+const contactPage = document.querySelector('.contact-page');
